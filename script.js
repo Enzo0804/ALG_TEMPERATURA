17afeb9c7b03f747aeb74095344138f5c1ff5f26
ENZO GONCALVES SANTOS <enzo.gsantos@senacsp.edu.br>
2024-09-25 16:33:00 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let Temp = parseInt(prompt("Quantos graus esta fazendo?"))
+
+if (Temp < 0 ) {
+    alert("Muito frio")
+    }else if (Temp <15 ) {
+        alert("Esta frio")
+    }else if (Temp <25 ) {
+        alert("Esta agradavel")
+    }else if (Temp <35 ) {
+        alert("Esta quente")
+    }else if (Temp >35 ) {
+        alert("Esta muito quente")
+        }
